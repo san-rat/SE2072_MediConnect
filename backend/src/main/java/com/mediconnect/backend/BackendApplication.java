1ab9fab98c03b0320ddc001bf43ca7e33e834acc
Luchithaj <it23631724@my.sliit.lk>
2025-08-26 22:15:27 +0530

    git test on backend

--- backend/src/main/java/com/mediconnect/backend/BackendApplication.java
+++ backend/src/main/java/com/mediconnect/backend/BackendApplication.java
@@ -3,6 +3,7 @@ package com.mediconnect.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//comment
 @SpringBootApplication
 public class BackendApplication {
 
