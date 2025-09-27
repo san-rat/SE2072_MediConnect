fea672dc949acf28fc1752efc9239d9356d406a5
Luchithaj <it23631724@my.sliit.lk>
2025-09-27 17:29:27 +0530

    unit testing done for User/Patient/Doctor services (Mockito)

---
+++ backend/src/test/java/com/mediconnect/SanityTest.java
@@ -0,0 +1,11 @@
+package com.mediconnect;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SanityTest {
+    @Test
+    void it_runs_tests() {
+        assertEquals(2, 1 + 1);
+    }
+}
