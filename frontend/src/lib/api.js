52aabce4fec65afba52063b0d829c356b4eafb59
san-rat <ratnayake.sanuk@gmail.com>
2025-11-17 18:47:25 +0530

    correct api.js

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8081", // backend URL
+  baseURL: "https://se2072mediconnect-production.up.railway.app", // backend URL
   withCredentials: true,            // if cookies are used
 });
 
